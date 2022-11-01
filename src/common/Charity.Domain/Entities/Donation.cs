e4babd8235e151a646eb1f0f2c879d7129c295a6
Guilherme Augusto Raduenz <graduenz@gmail.com>
2022-11-01 09:03:00 -0300

    setting nullables to entities

--- src/common/Charity.Domain/Entities/Donation.cs
+++ src/common/Charity.Domain/Entities/Donation.cs
@@ -7,9 +7,9 @@ public class Donation : BaseEntity
     public Guid OrganizationId { get; set; }
     public Guid DonatorId { get; set; }
     public decimal Value { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public DateTimeOffset DonatedAt { get; set; }
     
-    public virtual Donator Donator { get; set; }
-    public virtual Organization Organization { get; set; }
+    public virtual Donator? Donator { get; set; }
+    public virtual Organization? Organization { get; set; }
 }
--- src/common/Charity.Domain/Entities/Donator.cs
+++ src/common/Charity.Domain/Entities/Donator.cs
@@ -5,11 +5,11 @@ namespace Charity.Domain.Entities;
 
 public class Donator : BaseEntity
 {
-    public string Name { get; set; }
-    public string Document { get; set; }
+    public string? Name { get; set; }
+    public string? Document { get; set; }
     public DateTimeOffset DateOfBirth { get; set; }
-    public string Occupation { get; set; }
+    public string? Occupation { get; set; }
     public DonatorType Type { get; set; }
     
-    public virtual List<Donation> Donations { get; set; }
+    public virtual List<Donation>? Donations { get; set; }
 }
