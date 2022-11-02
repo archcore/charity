c2fe0102e5cacaed486439cb32724c57d4965e0a
Guilherme Augusto Raduenz <graduenz@gmail.com>
2022-11-02 00:04:43 -0300

    implemented DI setup

--- src/apps/Charity.Api/Program.cs
+++ src/apps/Charity.Api/Program.cs
@@ -1,3 +1,7 @@
+using Charity.Infrastructure;
+using Charity.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/common/Charity.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Charity.Application.Interfaces;
+using Charity.Infrastructure.Persistence;
+using Charity.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Charity.Infrastructure;
+
+public static class DependencyInjection
+{
+    private const string MigrationsAssembly = "Charity.Migrations";
+    
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) => services
+        .AddServiceImplementations()
+        .AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("ApplicationDbContext"),
+                b => b.MigrationsAssembly(MigrationsAssembly))
+        );
+
+    private static IServiceCollection AddServiceImplementations(this IServiceCollection services) => services
+        .AddScoped<IDonationService, DonationService>()
+        .AddScoped<IDonatorService, DonatorService>()
+        .AddScoped<IOrganizationService, OrganizationService>();
+}
