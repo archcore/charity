e9afcae0e49434dc25b6026d218fcbff4f7ab9e5
Guilherme Augusto Raduenz <graduenz@gmail.com>
2022-11-03 08:03:49 -0300

    added controller for Donator

---
+++ src/apps/Charity.Api/Controllers/DonatorsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using Charity.Api.Common.Controllers;
+using Charity.Api.Requests;
+using Charity.Application.Dto;
+using Charity.Application.Interfaces;
+using Charity.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Charity.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DonatorsController : BaseCrudController<Donator, DonatorDto, IDonatorService, DonatorPaginatedListRequest>
+{
+    public DonatorsController(IDonatorService service) : base(service)
+    {
+    }
+
+    protected override IEnumerable<Expression<Func<Donator, bool>>>? GetPaginatedListFilters(DonatorPaginatedListRequest request)
+    {
+        if (!string.IsNullOrEmpty(request.Name))
+            yield return d => EF.Functions.Like(d.Name, request.Name);
+        
+        if (!string.IsNullOrEmpty(request.Document))
+            yield return d => EF.Functions.Like(d.Document, request.Document);
+        
+        if (request.DateOfBirthMin.HasValue)
+            yield return d => d.DateOfBirth >= request.DateOfBirthMin.Value;
+        
+        if (request.DateOfBirthMax.HasValue)
+            yield return d => d.DateOfBirth <= request.DateOfBirthMax.Value;
+        
+        if (!string.IsNullOrEmpty(request.Occupation))
+            yield return d => EF.Functions.Like(d.Occupation, request.Occupation);
+        
+        if (request.Type.HasValue)
+            yield return d => d.Type == request.Type.Value;
+    }
+}
---
+++ src/apps/Charity.Api/Requests/DonatorPaginatedListRequest.cs
@@ -0,0 +1,13 @@
+﻿using Charity.Domain.Enums;
+
+namespace Charity.Api.Requests;
+
+public class DonatorPaginatedListRequest : PaginatedListRequest
+{
+    public string? Name { get; set; }
+    public string? Document { get; set; }
+    public DateTimeOffset? DateOfBirthMin { get; set; }
+    public DateTimeOffset? DateOfBirthMax { get; set; }
+    public string? Occupation { get; set; }
+    public DonatorType? Type { get; set; }
+}
