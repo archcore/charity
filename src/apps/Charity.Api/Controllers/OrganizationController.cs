53ca7ff5735d6a964afa780952c02423f017293b
Guilherme Augusto Raduenz <graduenz@gmail.com>
2022-11-03 08:11:39 -0300

    added controller for Organization

---
+++ src/apps/Charity.Api/Controllers/OrganizationController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using Charity.Api.Common.Controllers;
+using Charity.Api.Requests;
+using Charity.Application.Dto;
+using Charity.Application.Interfaces;
+using Charity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Charity.Api.Controllers;
+
+public class OrganizationController : BaseCrudController<Organization, OrganizationDto, IOrganizationService, OrganizationPaginatedListRequest>
+{
+    public OrganizationController(IOrganizationService service) : base(service)
+    {
+    }
+
+    protected override IEnumerable<Expression<Func<Organization, bool>>>? GetPaginatedListFilters(OrganizationPaginatedListRequest request)
+    {
+        if (!string.IsNullOrEmpty(request.LegalName))
+            yield return d => EF.Functions.Like(d.LegalName, request.LegalName);
+        
+        if (!string.IsNullOrEmpty(request.FriendlyName))
+            yield return d => EF.Functions.Like(d.FriendlyName, request.FriendlyName);
+        
+        if (!string.IsNullOrEmpty(request.Cause))
+            yield return d => EF.Functions.Like(d.Cause, request.Cause);
+        
+        if (!string.IsNullOrEmpty(request.Country))
+            yield return d => d.Country.Equals(request.Country, StringComparison.OrdinalIgnoreCase);
+        
+        if (request.FoundationDateMin.HasValue)
+            yield return d => d.FoundationDate >= request.FoundationDateMin.Value;
+        
+        if (request.FoundationDateMax.HasValue)
+            yield return d => d.FoundationDate <= request.FoundationDateMax.Value;
+
+        if (request.IsAcceptingDonations.HasValue)
+            yield return d => d.IsAcceptingDonations == request.IsAcceptingDonations.Value;
+    }
+}
---
+++ src/apps/Charity.Api/Requests/OrganizationPaginatedListRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Charity.Api.Requests;
+
+public class OrganizationPaginatedListRequest : PaginatedListRequest
+{
+    public string? LegalName { get; set; }
+    public string? FriendlyName { get; set; }
+    public string? Cause { get; set; }
+    public string? Country { get; set; }
+    public DateTimeOffset? FoundationDateMin { get; set; }
+    public DateTimeOffset? FoundationDateMax { get; set; }
+    public bool? IsAcceptingDonations { get; set; }
+}
