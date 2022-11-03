c7e96c2e7a6fd0ae80415c981a0b9358bc07648b
Guilherme Augusto Raduenz <graduenz@gmail.com>
2022-11-03 13:46:03 -0300

    changed filter adapter logic

--- src/common/Charity.Infrastructure/Common/Adapters/FilterAdapter.cs
+++ src/common/Charity.Infrastructure/Common/Adapters/FilterAdapter.cs
@@ -8,31 +8,12 @@ public class FilterAdapter : IFilterAdapter
     public IQueryable<TEntity> ApplyFilterExpressions<TEntity>(IQueryable<TEntity> queryable,
         IList<Expression<Func<TEntity, bool>>>? filters)
     {
-        return filters == null || filters.Count == 0
-            ? queryable
-            : ApplyFilterExpressionsInternal(queryable, filters);
-    }
+        if (filters == null || filters.Count == 0)
+            return queryable;
 
-    private static IQueryable<TEntity> ApplyFilterExpressionsInternal<TEntity>(IQueryable<TEntity> queryable,
-        IList<Expression<Func<TEntity, bool>>>? filters)
-    {
-        var parameter = Expression.Parameter(typeof(TEntity), "obj");
+        foreach (var filter in filters)
+            queryable = queryable.Where(filter);
 
-        var firstFilter = filters[0];
-        var filterExpression = Expression.Lambda<Func<TEntity, bool>>(firstFilter, parameter);
-
-        if (filters.Count > 1)
-        {
-            Expression outerExpression = filters[0];
-            for (var i = 1; i < filters.Count; ++i)
-            {
-                var aux = outerExpression;
-                outerExpression = Expression.And(aux, filters[i]);
-            }
-
-            filterExpression = Expression.Lambda<Func<TEntity, bool>>(outerExpression, parameter);
-        }
-
-        return queryable.Where(filterExpression);
+        return queryable;
     }
 }
