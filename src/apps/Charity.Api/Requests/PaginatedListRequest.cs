06254243a2c87321570543a69fc1c3c86826352a
Guilherme Augusto Raduenz <graduenz@gmail.com>
2022-11-03 00:43:07 -0300

    added standard model for paginated list request

---
+++ src/apps/Charity.Api/Requests/PaginatedListRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Charity.Api.Requests;
+
+public class PaginatedListRequest
+{
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public IDictionary<string, string>? Sort { get; set; }
+}
